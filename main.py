dcc46db553a44da4dcd53a06661e6cacd32acd37
vacobb <vacobb07@gmail.com>
2025-06-08 10:04:26 -0400

    Established 60 fps

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from constants import *
 
 def main():
     pygame.init()
+
+    # Creates window / play area
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        # Allows window to close properly
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
-        
+        dt = clock.tick(60) / 1000  # Set frame rate
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
